5d543f20e3d42099e8c35851a09b7780553334f1
Can Ersoy <can.ersoy@tum.de>
2023-09-07 11:53:11 +0300

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>
+    Talkative
+</h1>
+<div class="login-page">
+    <button>
+        Login
+    </button>
+    <button>
+        Register
+    </button>
+</div>
+
+<style>
+    .login-page{
+        left: 20%;
+        top: 50%;
+        position: absolute;
+        padding: 20px;
+        width: 50%;
+        border-radius: 20%;
+        height: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: end;
+    }
+</style>
